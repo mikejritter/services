83acebf1f80d2ac72662dc10698914fa70efe1b6
Michael Ritter <mike.ritter@lyrasis.org>
2025-04-10 12:57:03 -0600

    Rollback change to action code refname constants

--- services/movement/service/src/main/java/org/collectionspace/services/movement/nuxeo/MovementBotGardenConstants.java
+++ services/movement/service/src/main/java/org/collectionspace/services/movement/nuxeo/MovementBotGardenConstants.java
@@ -22,11 +22,11 @@ public class MovementBotGardenConstants {
 	public static final String ACTION_DATE_FIELD_NAME = "locationDate";
 
 	// See CollectionSpace wiki documentation for "RefName" values
-    // https://collectionspace.atlassian.net/wiki/spaces/DOC/pages/2754224504/RefName
-	public static final String DEAD_ACTION_CODE = "urn:NID:NAMESPACE:vocabularies:name(actionCode):item:name(actCode00)";
-	public static final String REVIVED_ACTION_CODE = "urn:NID:NAMESPACE:vocabularies:name(actionCode):item:name(actCode06)";
-	public static final String OTHER_ACTION_CODE = "urn:NID:NAMESPACE:vocabularies:name(actionCode):item:name(actCode05)";
-		
+	// https://collectionspace.atlassian.net/wiki/spaces/DOC/pages/2754224504/RefName
+	public static final String DEAD_ACTION_CODE = "urn:cspace:botgarden.cspace.berkeley.edu:vocabularies:name(actionCode):item:name(actCode00)";
+	public static final String REVIVED_ACTION_CODE = "urn:cspace:botgarden.cspace.berkeley.edu:vocabularies:name(actionCode):item:name(actCode06)";
+	public static final String OTHER_ACTION_CODE = "urn:cspace:botgarden.cspace.berkeley.edu:vocabularies:name(actionCode):item:name(actCode05)";
+
 	public static final String LABEL_REQUESTED_SCHEMA_NAME = BOTGARDEN_SCHEMA_NAME;
 	public static final String LABEL_REQUESTED_FIELD_NAME = "labelRequested";
 	public static final String LABEL_REQUESTED_YES_VALUE = "Yes";
