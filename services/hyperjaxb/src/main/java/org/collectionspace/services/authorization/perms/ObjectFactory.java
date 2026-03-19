2f5fb336696d20e3d194ffa3199dbb0166836e0c
Michael Ritter <mike.ritter@lyrasis.org>
2026-03-19 16:55:56 -0600

    Migrate hyperjaxb module from xsd to classes

---
+++ services/hyperjaxb/src/main/java/org/collectionspace/services/authorization/perms/ObjectFactory.java
@@ -0,0 +1,34 @@
+package org.collectionspace.services.authorization.perms;
+
+import javax.xml.namespace.QName;
+
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.annotation.XmlElementDecl;
+import jakarta.xml.bind.annotation.XmlRegistry;
+
+@XmlRegistry
+public class ObjectFactory {
+
+    private static final QName _Permission_QNAME = new QName("http://collectionspace.org/services/authorization/perms", "permission");
+
+    public ObjectFactory() {
+    }
+
+    public Permission createPermission() {
+        return new Permission();
+    }
+
+    public PermissionsList createPermissionsList() {
+        return new PermissionsList();
+    }
+
+    public PermissionAction createPermissionAction() {
+        return new PermissionAction();
+    }
+
+    @XmlElementDecl(namespace = "http://collectionspace.org/services/authorization/perms", name = "permission")
+    public JAXBElement<Permission> createPermission(Permission value) {
+        return new JAXBElement<>(_Permission_QNAME, Permission.class, null, value);
+    }
+
+}
